b422bd94f4c883c805453f43401837e6d9ecda5b
akkey <yusuke.akinari@gmail.com>
2025-06-28 16:46:24 +0900

    DataBaseのプログラムのリファクタリング

--- src/db.rs
+++ src/db.rs
@@ -1,13 +1,16 @@
 use serde::{Deserialize, Serialize};
-use mongodb::{Client, error::Result,};
+use mongodb::{Client, error::Result, Collection};
 use bson::{doc, oid::ObjectId};
 
 pub async fn get_blog_text(uri: &str, user_name: &str, title: &str) -> Result<Option<BlogText>> {
-    let client = Client::with_uri_str(uri).await?;
-    let database = client.database("blog_data");
-    let collection = database.collection::<BlogText>(user_name);
+    let collection = get_blog_collection(uri, user_name).await?;
     let filter = doc! { "title": title };
     collection.find_one(filter).await
+}
+async fn get_blog_collection(uri: &str, user_name: &str) -> Result<Collection<BlogText>> {
+    let client = Client::with_uri_str(uri).await?;
+    let database = client.database("blog_data");
+    Ok(database.collection::<BlogText>(&format!("user_name: {user_name}")))
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
