2e8c984117e00ad55fdf5becc308b25c083c8b75
akkey <yusuke.akinari@gmail.com>
2025-06-29 08:54:50 +0900

    DataBaseの関数をUserName型を使ったものに変更

--- src/domain/models/user_name.rs
+++ src/domain/models/user_name.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, PartialEq, Eq, Deserialize, Serialize)]
@@ -8,5 +10,10 @@ impl TryFrom<String> for UserName {
     type Error = anyhow::Error;
     fn try_from(inner: String) -> anyhow::Result<Self> {
         Ok(UserName { inner }) //とりあえず素通し
+    }
+}
+impl fmt::Display for UserName{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str(&self.inner)
     }
 }
