c0d8c3b432e6c1a698fb1d596fe95e3757a1ed0a
akkey <yusuke.akinari@gmail.com>
2025-06-28 15:01:14 +0900

    ブログテキストの取得処理

---
+++ src/db.rs
@@ -0,0 +1,18 @@
+use serde::{Deserialize, Serialize};
+use mongodb::{bson::Uuid, Client, error::Result};
+use bson::doc;
+
+pub async fn get_blog_text(uri: &str, user_name: &str, title: &str) -> Result<Option<BlogText>> {
+    let client = Client::with_uri_str(uri).await?;
+    let database = client.database("blog_data");
+    let collection = database.collection::<BlogText>(user_name);
+    let filter = doc! { "title": title };
+    collection.find_one(filter).await
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct BlogText {
+    id: Uuid,
+    title: String,
+    content: String
+}
