073a49ec8e38a01e982255d44e566983958e0e63
akkey <yusuke.akinari@gmail.com>
2025-06-29 09:23:53 +0900

    Furaukutaさんのプログラムに合わせて、ArticleRepositoryのメソッドを追加

--- src/domain/repositorys/article_repository.rs
+++ src/domain/repositorys/article_repository.rs
@@ -1,4 +1,5 @@
 use async_trait::async_trait;
+use bson::oid::ObjectId;
 
 use crate::domain::models::{article::Article, user_name::UserName};
 
@@ -6,8 +7,13 @@ use crate::domain::models::{article::Article, user_name::UserName};
 #[async_trait]
 pub trait ArticleRepositiory {
     /// 新しい記事を追加するメソッド
-    async fn add_article(&self, article: Article) -> anyhow::Result<()>;
-    /// titleとユーザー名を元に、記事を手に入れる関数
+    async fn add_article(&self, article: Article) -> anyhow::Result<ObjectId>;
+    /// titleとユーザー名を元に、記事を手に入れるメソッド
     /// 記事が存在しなかった場合は、Ok(None)を返す。
     async fn get_article(&self, author: UserName, title: &str) -> anyhow::Result<Option<Article>>;
+    /// idを元に記事を手に入れるメソッド
+    /// 記事が存在しなかった場合は、Ok(None)を返す。
+    async fn get_article_from_id(&self, id: ObjectId) -> anyhow::Result<Option<Article>>;
+    /// 記事の更新をするメソッド
+    async fn update_page(&self, id: ObjectId, title: Option<String>, content: Option<String>) -> anyhow::Result<()>;
 }
