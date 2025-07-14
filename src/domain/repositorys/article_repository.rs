4f1565e35e9f8364de9970dfb8af977129ed78b7
akkey <yusuke.akinari@gmail.com>
2025-07-14 23:15:46 +0900

    ドキュメンテーションコメントの修正

--- src/domain/repositorys/article_repository.rs
+++ src/domain/repositorys/article_repository.rs
@@ -9,7 +9,7 @@ pub trait ArticleRepository {
     /// `skip`: 取得開始位置, `limit`: 最大取得数
     /// 
     /// # Errors
-    /// 記事の情報にアクセスできなかった場合は`Err(Error)`を返す
+    /// 記事の情報にアクセスできなかった場合は`Err`を返す
     async fn get_articles(&self, skip: usize, limit: usize) -> Result<Vec<Article>, ArticleServiceError>;
 
     /// IDを元に記事を取得する
@@ -18,7 +18,7 @@ pub trait ArticleRepository {
     /// 記事が存在する場合は`Ok(Some(Article))`を返す
     /// 
     /// # Errors
-    /// データベースへのアクセスに失敗した場合は`Err(Error)`を返す
+    /// データベースへのアクセスに失敗した場合は`Err`を返す
     async fn get_article_by_id(&self, id: ObjectId) -> Result<Option<Article>, ArticleServiceError>;
 
     /// 新しい記事を追加する
@@ -26,24 +26,24 @@ pub trait ArticleRepository {
     /// 追加された記事を返す
     /// 
     /// # Errors
-    /// 記事の追加に失敗した場合は`Err(Error)`を返す
+    /// 記事の追加に失敗した場合は`Err`を返す
     async fn add_article(&self, title: String, author: UserName, content: String) -> Result<Article, ArticleServiceError>;
 
     /// 記事を更新する
     /// `id`: 記事のID, `title`: 新しいタイトル, `content`: 新しい内容
-    /// 更新が成功した場合は`Ok(Article)`, 失敗した場合は`Err(Error)`
+    /// 更新が成功した場合は`Ok(Article)`
     /// `title`と`content`のいずれかが`None`の場合は更新しない
     /// 
     /// # Errors
-    /// 記事が存在しない場合や、データベースへのアクセスに失敗した場合は`Err(Error)`を返す
+    /// 記事が存在しない場合や、データベースへのアクセスに失敗した場合は`Err`を返す
     async fn update_article(&self, id: ObjectId, title: Option<String>, content: Option<String>) -> Result<Article, ArticleServiceError>;
 
     /// 記事を削除する
     /// `id`: 記事のID
-    /// 削除が成功した場合は`Ok(())`, 失敗した場合は`Err(Error)`
+    /// 削除が成功した場合は`Ok(())`
     /// 
     /// # Errors
-    /// 記事が存在しない場合や、データベースへのアクセスに失敗した場合は`Err(Error)`を返す
+    /// 記事が存在しない場合や、データベースへのアクセスに失敗した場合は`Err`を返す
     async fn delete_article(&self, id: ObjectId) -> Result<(), ArticleServiceError>;
 
     /// クエリを元に記事を取得する
@@ -51,6 +51,6 @@ pub trait ArticleRepository {
     /// クエリにはタイトルや著者名などが含まれる
     /// 
     /// # Errors
-    /// データベースへのアクセスに失敗した場合は`Err(Error)`を返す
+    /// データベースへのアクセスに失敗した場合は`Err`を返す
     async fn get_articles_with_query(&self, skip: usize, limit: usize, query: ArticleQuery) -> Result<Vec<Article>, ArticleServiceError>;
 }
